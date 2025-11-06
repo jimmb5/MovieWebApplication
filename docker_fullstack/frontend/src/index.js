606799ffee28e5040993e85159cf164233aa7236
jimmb5 <jimmy.bergbacka@gmail.com>
2025-11-06 11:37:06 +0200

    navbar

--- docker_fullstack/frontend/src/index.js
+++ docker_fullstack/frontend/src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./variables.css";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
